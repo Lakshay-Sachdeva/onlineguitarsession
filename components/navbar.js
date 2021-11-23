920e0e62689592f54cdfb00fa5a88a27a8433899
Lakshay Sachdeva <lakshay07sachdeva@gmail.com>
2021-11-24 01:53:42 +0530

    navigation components, style remains

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,16 @@
 function navbar(){
-    return ``
+    return `
+    <div>
+        <h1>TRUE GUITAR LEARNING</h1>
+    </div>
+    <div>
+        <div>Home</div>
+        <div>About</div>
+        <div>Services</div>
+        <div>Contact</div>
+        <button>Get Started</button>
+    </div>
+    `
 }
 
 export default navbar ;
